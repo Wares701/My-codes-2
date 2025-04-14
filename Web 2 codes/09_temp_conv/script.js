f9e1602f6b40330cbebfbe02f7ca69613b718f93
Wares701 <Elhamahmadnasiri@gmail.com>
2025-04-14 09:53:03 -0700

    In class added steps

--- Web 2 codes/09_temp_conv/script.js
+++ Web 2 codes/09_temp_conv/script.js
@@ -1,2 +1,23 @@
 
 result_c.innerHTML = "39";
+
+/*
+    1 function, not 3
+    add event listener to textbox (enter key)
+    
+
+
+    <select id="myDropdown">
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+    </select>
+
+    // JavaScript
+    const dropdown = document.getElementById('myDropdown');
+    dropdown.addEventListener('change', function() {
+        const selectedValue = this.value;
+        console.log(selectedValue); // Outputs the selected value when changed
+    });
+
+*/
