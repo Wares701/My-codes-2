953a8a84a924692aa54e9c1a89159af201529a84
Wares701 <Elhamahmadnasiri@gmail.com>
2025-04-24 11:30:59 -0700

    Made all RPS

--- Web 2 codes/15_virus/script.js
+++ Web 2 codes/15_virus/script.js
@@ -17,12 +17,57 @@
 function missionSuccess(e) 
 {
     console.log(e.target.id);
+    if(downloadTimer == null)
+    {
+        downloadTimer = setInterval(addWidth, 1000 / 5);
+    }
+}
+//Another function for the alarm itslef
+function addWidth()
+{
+    perc += random_range(1,10)/10;
+    console.log(perc);
+    progressBox.style.width = perc+"%";
+    if (perc < 10) {
+        outputBox.innerHTML = "Download Started";
+    } else if (perc < 20) {
+        outputBox.innerHTML = "By the way... this is a virus Hahahahah!";
+    } else if (perc < 30) {
+        outputBox.innerHTML = "You fell into my trap";
+    } else if (perc < 40) {
+        outputBox.innerHTML = "Just kidding... or am I?";
+    } else if (perc < 50) {
+        outputBox.innerHTML = "50% complete... there's no going back now.";
+    } else if (perc < 60) {
+        outputBox.innerHTML = "Your files are looking tasty.";
+    } else if (perc < 70) {
+        outputBox.innerHTML = "Encrypting your memes...";
+    } else if (perc < 80) {
+        outputBox.innerHTML = "Summoning digital gremlins.";
+    } else if (perc < 90) {
+        outputBox.innerHTML = "Installing chaos.exe";
+    } else if (perc < 100) {
+        outputBox.innerHTML = "Finalizing your doom... almost there!";
+    }
+    else if(perc >= 100)
+    {
+        clearInterval(downloadTimer);
+        downloadTimer = null;
+        perc = 0;
+        outputBox.innerHTML = "Download complete. You are pwned";
+    }
 }
 
+function random_range(low, high)
+{
+  let size = high - low + 1;
+  return Math.floor(Math.random()*size)+low;
+}
 let startDownload = document.getElementById("start");
 let outputBox = document.getElementById("output");
 let progressBox = document.getElementById("progress")
 
+//Event Listeners
 startDownload.addEventListener("click", missionSuccess);
 
 downloadTimer = null;
