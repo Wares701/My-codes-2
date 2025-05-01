3e372618d7c5800b82105e84607d3d4f665b2de5
Wares701 <Elhamahmadnasiri@gmail.com>
2025-05-01 11:31:07 -0700

    almost finished

--- Web 2 codes/10_RPS/script.js
+++ Web 2 codes/10_RPS/script.js
@@ -1,35 +1,74 @@
+let Rock = document.getElementById("rock");
+let Paper = document.getElementById("paper");
+let Scissors = document.getElementById("scissors");
+let outputBox = document.getElementById("result");
+let userScore = document.getElementById("user_score");
+let computerScore = document.getElementById("computer_score");
+let bothScored = document.getElementById("both_scored");
+let computerSelect = document.getElementById("computer");
+let userSelect = document.getElementById("user");
+let compWinSentence = "Boom! Crushed it. Better luck next time human!";
 
 function computerFunction(e)
 {
   console.log(e.target);
     let rand = random_range(1,3)
+    console.log(rand)
     if (rand == 1)
     {
         computerSelect.innerHTML = "Rock";
     } 
     else if(rand == 2) 
     {
-        computerSelect.innerHTML = "Papper"
+        computerSelect.innerHTML = "Paper"
     }
     else if(rand == 3) 
     {
         computerSelect.innerHTML = "Scissors"
     }
 
+    if(userSelect.innerHTML == "Paper" && rand == 2)
+    {
+      outputBox.innerHTML = "Guess it's a tie.";
+    }
+    else if(userSelect.innerHTML == "Rock" && rand == 1)
+    {
+      outputBox.innerHTML = "Guess it's a tie.";
+    }
+    else if(userSelect.innerHTML == "Scissors" && rand == 3)
+    {
+      outputBox.innerHTML = "Guess it's a tie.";
+    }
+   else if(userSelect.innerHTML == "Scissors" && rand == 2)
+    {
+      outputBox.innerHTML = "You win this one... but I'll crush the next one!";
+    }
+    else if(userSelect.innerHTML == "Rock" && rand == 3)
+    {
+      outputBox.innerHTML = "You win this one... but I'll crush the next one!";
+    }
+    else if(userSelect.innerHTML == "Paper" && rand == 1)
+    {
+      outputBox.innerHTML = "You win this one... but I'll crush the next one!";
+    }
 
-    /*
-      If both the user and computer select the same thing, then no one wins.
-      If 
-        the user selects rock and the computer selects scissors 
-        or user selects scissors and the computer selects paper, 
-        or the user selects paper and the computer selects rock, 
-            then the user wins.
-      else, the computer wins.
-    */
+    else
+    {
+      outputBox.innerHTML = compWinSentence;
+    }
 
 
+    let count = 0;
+    for (let i = 0; i < yourArray.length; i++) {
+      if (yourArray[i] === compWinSentence) {
+        count++;
+      }
+    }
 }
 
+function counter(){
+  
+}
 function paperPress(e)
 {
     userSelect.innerHTML = "Paper";
@@ -43,28 +82,18 @@ function scissorsPress(e)
     userSelect.innerHTML = "Scissors";
 }
 
-
-
 function random_range(low, high)
 {
   let size = high - low + 1;
   return Math.floor(Math.random()*size)+low;
 }
 
-let Rock = document.getElementById("rock");
-let Paper = document.getElementById("paper");
-let Scissors = document.getElementById("scissors");
-let outputBox = document.getElementById("result");
-let userScore = document.getElementById("user_score");
-let computerScore = document.getElementById("computer_score");
-let bothScored = document.getElementById("both_scored");
-let computerSelect = document.getElementById("computer");
-let userSelect = document.getElementById("user");
 
 
-Rock.addEventListener("click", computerFunction);
 Rock.addEventListener("click", rockPress);
-Paper.addEventListener("click", computerFunction);
+Rock.addEventListener("click", computerFunction);
 Paper.addEventListener("click", paperPress);
-Scissors.addEventListener("click", computerFunction);
+Paper.addEventListener("click", computerFunction);
 Scissors.addEventListener("click", scissorsPress);
+Scissors.addEventListener("click", computerFunction);
+
